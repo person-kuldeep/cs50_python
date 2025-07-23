f1da250b49d0a3f66f162cb22c513fa294d48112
helokd <person.kuldeep@gmail.com>
2025-07-23 13:34:07 +0530

    c02

---
+++ week0/lacture/helloworld.py
@@ -0,0 +1,19 @@
+# make a variable for greeting
+# greet = "hello, world!!"
+
+# print the variable 
+# print(greet , sep="hess", end="!!\"\n")
+
+# input from the user
+
+name = input("whats your name? ")
+
+# striping whitespaces and capitalizing every word
+name = name.strip().title() # .captitalze() converts first letter of the str to uppercase
+
+# print the greeting
+
+print(f"Hello, {name} !")
+
+
+
