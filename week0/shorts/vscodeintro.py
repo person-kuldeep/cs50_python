e5a8878fe1c9fb8b187a11abcab499f7976f28bf
helokd <person.kuldeep@gmail.com>
2025-08-07 20:07:18 +0530

    c06_vscodeintro

---
+++ week0/shorts/vscodeintro.py
@@ -0,0 +1,11 @@
+# code . and code <filename> to open file in vs code
+# ls  to list files in current dir
+# pwd to print working directory
+# cd <dir> to change directory
+# mkdir <dir> to make a new directory
+# rm <file> to remove a file
+# rmdir <dir> to remove a directory
+# cp <file> <newfile> to copy a file
+# mv <file> <newfile> to move or rename a file
+# ..  represents the parent directory
+# .   represents the current directory
