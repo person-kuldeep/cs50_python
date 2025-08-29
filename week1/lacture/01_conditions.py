737bb046b5e0493a1c93e27e2a9f63ce64806b12
person-kuldeep <person.kuldeep@gmail.com>
2025-08-29 14:14:47 +0530

    c10_w1_coditions

---
+++ week1/lacture/01_conditions.py
@@ -0,0 +1,40 @@
+# conditions
+# agree for the insurance policy
+"""
+s = input("did you approve for the policy? (y/n): ")
+if s == 'y':
+    print("you can get the insurance")  
+elif s == 'n':
+    print("you cannot get the insurance")
+else:
+    print("invalid input")        
+
+"""
+# your grade is
+"""
+g = int(input("enter your grade: "))
+
+if g >= 90:
+    print("A")
+elif g >= 80:
+    print("B")
+elif g >= 70:
+    print("C")
+else :
+    print("F")       
+"""
+
+# even or odd 
+def main():
+    n = int(input("enter a number: "))
+    if is_even(n):
+        print("even")
+    else:
+        print("odd")   
+
+
+def is_even(x):
+    #return True if x % 2 == 0 else False
+    return x % 2 == 0
+
+main()
